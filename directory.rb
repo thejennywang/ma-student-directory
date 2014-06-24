00c136da620eaf3bd6a063a9c4036691df9fe0eb
Nicola Aitken <naitken22@gmail.com>
2014-06-24 13:50:36 +0100

    added an array

--- directory.rb
+++ directory.rb
@@ -1,33 +1,40 @@
-student_count = 25
+
+#Lets put all of the students into an array
+students = [
+	"Dave",
+	"Eddie",
+	"Catharina",
+	"Igor",
+	"Marco",
+	"Lisa",
+	"Michiel",
+	"Jean",
+	"Nicola",
+	"Jenny",
+	"Iona",
+	"Nikesh",
+	"Chloe",
+	"Toan",
+	"Jamie",
+	"Peter",
+	"Talal",
+	"Charlie",
+	"Charlotte",
+	"Thomas",
+	"Zoe",
+	"Hannah",
+	"Joe",
+	"Alex",
+	"Jeremy",
+]
+
 #first we're gonna print the students' names
 puts "The students of my cohort at Makers Academy"
 puts "-----------------"
-puts "Dave"
-puts "Eddie"
-puts "Catharina"
-puts "Igor"
-puts "Marco"
-puts "Lisa"
-puts "Michiel"
-puts "Jean"
-puts "Nicola"
-puts "Jenny"
-puts "Iona"
-puts "Nikesh"
-puts "Chloe"
-puts "Toan"
-puts "Jamie"
-puts "Peter"
-puts "Talal"
-puts "Charlie"
-puts "Charlotte"
-puts "Thomas"
-puts "Zoe"
-puts "Hannah"
-puts "Joe"
-puts "Alex"
-puts "Jeremy"
+students.each do |student|
+	puts student
+end
 
 #finally, we print the total
-print "Overall, we have #{student_count} great students"
+print "Overall, we have #{students.length} great students"
 
