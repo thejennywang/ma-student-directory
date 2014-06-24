29eeeed291703526e53e76dbac5b6e8f6657016a
Jenny Wang <jennywang@THEALMIGHTYJENNY.local>
2014-06-24 16:39:11 +0100

    Jenny & Nicola #winnin'

--- directory.rb
+++ directory.rb
@@ -31,23 +31,23 @@ students = [
 =end
 
 def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "-----------------"
+	print "The students of my cohort at Makers Academy\n"
+	print "-----------------\n"
 end
 
-def print(students)
+def display(students)
 	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+		print "#{student[:name]} (#{student[:cohort]} cohort)\n"
 	end
 end 
 
 def print_footer(names)
-	puts "Overall, we have #{names.length} great students"
+	print "Overall, we have #{names.length} great students\n"
 end
 
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	print "Please enter the names of the students\n"
+	print "To finish, just hit return twice\n"
 	#create an empty array
 	students = []
 	#get the first name
@@ -56,7 +56,7 @@ def input_students
 	while !name.empty? do
 		#add the studen hash to the array
 		students << {:name => name, :cohort => "June 2014"}
-		puts "Now we have #{students.length} students"
+		print "Now we have #{students.length} students\n"
 		#gets another name from the user
 		name = gets.chomp
 	end
@@ -67,5 +67,5 @@ end
 #nothing happens until we call the methods
 students = input_students
 print_header
-print(students)
+display(students)
 print_footer(students)
