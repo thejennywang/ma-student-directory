f82442e1b625946c1bcc03cfee4c4b524bffbea1
Jenny Wang <jennywang@THEALMIGHTYJENNY.local>
2014-06-25 17:51:37 +0100

    Nailed Exercise 9 AGAIN. BOOM.

--- directory.rb
+++ directory.rb
@@ -1,52 +1,16 @@
-
-#Lets put all of the students into an array
-=begin
-students = [
-	{:name => "Dave Bayon", :cohort => :june},
-	{:name => "Eddie Andress", :cohort => :june},
-	{:name => "Catharina Luebke-Detring", :cohort => :june},
-	{:name => "Igor Borowski", :cohort => :june},
-	{:name => "Marco Araujo", :cohort => :june},
-	{:name => "Lisa Altmann-Richer", :cohort => :june},
-	{:name => "Michiel Stigter", :cohort => :june},
-	{:name => "Jean Troiani", :cohort => :june},
-	{:name => "Nicola Aitken", :cohort => :june},
-	{:name => "Jenny Wang", :cohort => :june},
-	{:name => "Iona Seligman", :cohort => :june},
-	{:name => "Nikesh Ashar", :cohort => :june},
-	{:name => "Chloe Donegan", :cohort => :june},
-	{:name => "Toan Nguyen", :cohort => :june},
-	{:name => "Jamie Allan", :cohort => :june},
-	{:name => "Peter McCarthy", :cohort => :june},
-	{:name => "Talal Khodr", :cohort => :june},
-	{:name => "Charlie Redwood", :cohort => :june},
-	{:name => "Charlotte Kelly", :cohort => :june},
-	{:name => "Thomas Beeley", :cohort => :june},
-	{:name => "Zoe Bryant", :cohort => :june},
-	{:name => "Hannah Waxman", :cohort => :june},
-	{:name => "Joe Dowdell", :cohort => :june},
-	{:name => "Alex Z", :cohort => :june},
-	{:name => "Jeremy Fox", :cohort => :june},
-]
-=end
-
 def print_header
 	print "The students of my cohort at Makers Academy\n"
 	print "-----------------\n"
 end
 
 def display(students)
-	students.map{|student| student[:cohort]}.uniq.each do |cohort|
-		puts "The students for the #{cohort.to_s} cohorts are:"
-		students.each do |student|
-			puts "#{student[:name]}, age #{student[:age]}, #{student[:cohort]}\n".center(80) if student[:cohort] == cohort
+	cohort = students.map { |student| student[:cohort] }.uniq 
+	cohort.each do |month|
+		puts "The students in the #{month.to_s} cohort are:" 
+			students.each do |student|
+				puts "#{student[:name]}, #{student[:age]}, #{student[:cohort]}" if student[:cohort] == month
+			end
 	end
-end
-	# i = 0
-	# while i < students.length
-	# 	print "#{students[i][:name]}, age #{students[i][:age]}, #{students[i][:cohort]}\n".center(80)
-	# 	i+=1
-	# end
 end
 
 def print_footer(names)
