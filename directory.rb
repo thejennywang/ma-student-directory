ca3a916c1b5842d0e00affb978a911a2e45dff5e
Jenny Wang <jennywang@THEALMIGHTYJENNY.local>
2014-06-26 12:13:35 +0100

    Jenny, Jeremy, Pete | Added a beautiful, GLORIOUS Interative menu

--- directory.rb
+++ directory.rb
@@ -58,7 +58,7 @@ def input_students
 		print "Now we have #{students.length} student#{students.length > 1 ? "s" : ""}\n" 
 		
 		#gets another name from the user
-		print "Please enter your name OR press return to exit\n"
+		print "Please enter another name OR press return for Menu\n"
 		name = gets.gsub("\n", "")
 	elsif confirmation == "n"
 		puts "Please re-enter your name"
@@ -69,8 +69,32 @@ end
 	students
 end
 
+def interactive_menu
+	students = []
+	loop do
+		# 1. print the menu and ask the user what to do
+		puts "--------MENU---------"
+		puts "1. Input the students"
+		puts "2. Show the students"
+		puts "9. Exit" # 9 because we'll be adding more items
+		puts "---------------------"
+		# 2. read the input and save it into a variable
+		selection = gets.chomp
+		# 3. do what the user has asked
+		case selection
+		  when "1"
+			students = input_students
+		  when "2"
+			print_header
+			display(students)
+			print_footer(students)
+		  when "9"
+			exit # this will cause the program to terminate
+		  else
+			puts "I don't know what you meant, try again"
+		end
+	end
+end
+
 #nothing happens until we call the methods
-students = input_students
-print_header
-display(students)
-print_footer(students)
+interactive_menu
