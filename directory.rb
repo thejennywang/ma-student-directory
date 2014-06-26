8c992f9d9076dcccbfa5981a0e8788e95437f26a
Jenny Wang <jennywang@THEALMIGHTYJENNY.local>
2014-06-26 14:25:45 +0100

    Jenny, Jeremy, Peter | Added students.csv and gitignore files

--- directory.rb
+++ directory.rb
@@ -71,6 +71,7 @@ def print_menu
 		puts "\n--------MENU---------"
 		puts "1. Input the students"
 		puts "2. Show the students"
+		puts "3. Save the list to students.csv"
 		puts "9. Exit" # 9 because we'll be adding more items
 		puts "---------------------\n"
 end
@@ -83,17 +84,32 @@ end
 
 def process(selection)
 	case selection
-	  when "1"
-		@students = input_students
-	  when "2"
-		show_students
-	  when "9"
-		exit # this will cause the program to terminate
-	  else
+		when "1"
+			@students = input_students
+		when "2"
+			show_students
+		when "3"
+			save_students
+		when "9"
+			exit # this will cause the program to terminate
+	else
 		puts "I don't know what you meant, try again"
 	end
 end
 
+def save_students
+	# open the file for writing
+	file = File.open("students.csv", "w")
+	# iterate over the array of students
+	@students.each do |student|
+		# student_data = [student[:name], student[:cohort]]
+		# csv_line = student_data.join(",")
+		# file.puts csv_line
+		file.puts [student[:name], student[:age], student[:cohort]].join(", ")
+
+	end
+	file.close
+end
 
 def interactive_menu
 	@students = []
